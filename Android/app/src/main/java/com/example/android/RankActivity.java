ac7243327316aaf19132b5882cf492fbb0172684
vrdd21 <vrdd21@naver.com>
2020-05-01 18:22:50 +0900

    Ranking view updated

--- Android/app/src/main/java/com/example/android/RankActivity.java
+++ Android/app/src/main/java/com/example/android/RankActivity.java
@@ -1,13 +1,19 @@
 package com.example.android;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RankActivity extends AppCompatActivity {
+
+    private ScrollView scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,29 @@ public class RankActivity extends AppCompatActivity {
         view.setLayoutManager(layoutManager);
         view.setAdapter(recyclerViewAdapter);
 
+
+        scrollView = (ScrollView) findViewById(R.id.ranking_scrollbar);
+
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        layoutTheView();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        layoutTheView();
+    }
+
+    private void layoutTheView() {
+        ActionBar actionBar = this.getSupportActionBar();
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) scrollView.getLayoutParams();
+        int actionBarHeight = actionBar.getHeight();
+        params.setMargins(20, actionBarHeight + 30, 20, 0);
+        scrollView.setLayoutParams(params);
     }
 }
