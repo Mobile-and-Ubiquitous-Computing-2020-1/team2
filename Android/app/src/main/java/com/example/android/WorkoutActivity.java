445218a88db09a8049a528b954c80f501220a4dc
Hyunwoo Jung <chaosphch@gmail.com>
2020-05-04 16:33:36 +0900

    Create camera preview

--- Android/app/src/main/java/com/example/android/WorkoutActivity.java
+++ Android/app/src/main/java/com/example/android/WorkoutActivity.java
@@ -6,17 +6,30 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.widget.FrameLayout;
 
 public class WorkoutActivity extends AppCompatActivity {
     private static final String TAG = WorkoutActivity.class.getSimpleName();
     private static final int MY_PERMISSION_REQUEST_CAMERA = 0;
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+        requestCameraPermission(); // Move to global main activity
+        mCamera = getCameraInstance();
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    private void requestCameraPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
@@ -25,5 +38,15 @@ public class WorkoutActivity extends AppCompatActivity {
         } else {
             Log.d(TAG, "permitted");
         }
+    }
+
+    private Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e) {
+            Log.d(TAG, "getCameraInstance: No available camera");
+        }
+        return c;
     }
 }
