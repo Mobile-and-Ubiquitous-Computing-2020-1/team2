26ac29740200ec7560f5811eaf43c1208c1df787
Hyunwoo Jung <chaosphch@gmail.com>
2020-05-04 15:56:20 +0900

    Add camera permission prompt

--- Android/app/src/main/java/com/example/android/WorkoutActivity.java
+++ Android/app/src/main/java/com/example/android/WorkoutActivity.java
@@ -1,14 +1,29 @@
 package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class WorkoutActivity extends AppCompatActivity {
+    private static final String TAG = WorkoutActivity.class.getSimpleName();
+    private static final int MY_PERMISSION_REQUEST_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.CAMERA},
+                    MY_PERMISSION_REQUEST_CAMERA);
+        } else {
+            Log.d(TAG, "permitted");
+        }
     }
 }
