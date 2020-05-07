be2c5a55abd747f933ba825ec71dec26594f38a3
JeonChangMin <imsameperson@gmail.com>
2020-05-07 21:54:54 +0900

    Login with firebase

---
+++ android/app/src/main/java/com/example/android/User.java
@@ -0,0 +1,21 @@
+package com.example.android;
+
+import com.google.firebase.database.PropertyName;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class User {
+    @PropertyName("id")
+    public String id = null;
+    @PropertyName("friends_id")
+    public List<String> friends_id = new LinkedList<>();
+    @PropertyName("scores")
+    public List<Integer> scores = new LinkedList<>();
+
+    User() {}
+
+    User(String id) {
+        this.id = id;
+    }
+}
